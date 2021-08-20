0f958a0c5e5450f01ac7f4797a1aa092f51501ab
Tung Pham <tung.42@gmail.com>
2021-08-20 19:06:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if (isset($_POST["submitForm"])) {
     <footer class="mt-auto text-white-50 bg-dark p-3">
         <div class="container">
             <div class="row">
-                <p>&copy; Bản quyền <a href="https://tungpham42.github.io/" class="text-white">Phạm Tùng</a></p>
+                <p>&copy; Bản quyền thuộc về <a href="https://tungpham42.github.io/" class="text-white">Phạm Tùng</a></p>
             </div>
         </div>
     </footer>
